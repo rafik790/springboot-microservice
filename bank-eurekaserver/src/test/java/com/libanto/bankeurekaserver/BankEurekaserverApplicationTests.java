c8191a6e2ab36494d0b5c935c5bb4a9d83edf96a
Rafik Mondal <RafikM@interrait.com>
2023-01-08 22:12:49 +0530

    Added Eureka Server

---
+++ bank-eurekaserver/src/test/java/com/libanto/bankeurekaserver/BankEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libanto.bankeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankEurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
