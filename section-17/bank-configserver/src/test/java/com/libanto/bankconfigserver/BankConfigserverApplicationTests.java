c592f0da561aee8023be019a44ad6ad751fac017
Rafik Mondal <RafikM@interrait.com>
2023-02-11 21:26:44 +0530

    Securing Microservices using OAuth2 client credentials grant flow

---
+++ section-17/bank-configserver/src/test/java/com/libanto/bankconfigserver/BankConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libanto.bankconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ section-17/bank-gatewayserver/src/test/java/com/libanto/bankgatewayserver/BankGatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libanto.bankgatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankGatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
